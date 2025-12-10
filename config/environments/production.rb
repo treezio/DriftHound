5c61bbaf20760995ceca010effce1f6a7dc27ada
Luis Fernández <luis.fernandez@sequra.es>
2025-12-10 16:58:46 +0100

    fix: Configure production mode for basic app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Disable image variant processing since we don't use image uploads
+  config.active_storage.variant_processor = :disabled
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
